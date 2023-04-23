526150668171bd00af49121204f61f86339932db
Arielqqq <ariel.wu.94@gmail.com>
2023-04-23 14:18:53 +1000

    Learning OOP: Refactoring readNumber method to Console.java.

--- CalculateMortgage/src/main/java/com/mortgage/Main.java
+++ CalculateMortgage/src/main/java/com/mortgage/Main.java
@@ -1,25 +1,31 @@
 package com.mortgage;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.text.NumberFormat;
-import java.util.Scanner;
 
-public class CalculateMortgage {
+public class Main {
     final static byte MONTHS_IN_YEAR = 12;
     final static byte PERCENT = 100;
 
     public static void main(String[] args) {
-        int principal = (int) readNumber("Principal:", 1000, 1_000_000);
-        float annualInterest = (float) readNumber("Annual Interest Rate:", 1, 30);
-        byte years = (byte) readNumber("Period (Years):", 1, 30);
+        int principal = (int) Console.readNumber("Principal:", 1000, 1_000_000);
+        float annualInterest = (float) Console.readNumber("Annual Interest Rate:", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Years):", 1, 30);
+        //var calculateMortgage = new calculateMortgage1();
+        //calculateMortgage.mortgage();
+        printMortgage(principal, annualInterest, years);
+        printPaymentSchedule(principal, annualInterest, years);
+    }
 
+    private static void printMortgage(int principal, float annualInterest, byte years) {
         double mortgage = calculateMortgage(principal, annualInterest, years);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("Mortgage");
         System.out.println("--------");
         System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
 
+    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
@@ -27,28 +33,8 @@ public class CalculateMortgage {
             double balance = calculateBalance(principal, annualInterest, years, month);
             System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
-
-        //printMortgage(principal, annualInterest, years);
     }
 
-    private static void printMortgage(int principal, float annualInterest, byte years) {
-        double mortgage = calculateMortgage(principal, annualInterest, years);
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.print("Mortgage:" + mortgageFormatted);
-    }
-
-    public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
-        double value;
-        while (true) {
-            System.out.print(prompt);
-            value = scanner.nextFloat();
-            if (value >= min && value <= max)
-                break;
-            System.out.println("Enter a value between" + min + "and" + max);
-        }
-        return value;
-    }
 
     public static double calculateBalance(
             int principal,
@@ -72,7 +58,10 @@ public class CalculateMortgage {
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
         float numberOfPayments = years * MONTHS_IN_YEAR;
 
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
         return mortgage;
     }
+
 }
