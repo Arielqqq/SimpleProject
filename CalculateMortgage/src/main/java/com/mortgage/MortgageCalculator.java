eca02eab7ae7198af2b66f712e12170b42fffbab
Arielqqq <ariel.wu.94@gmail.com>
2023-05-01 18:34:14 +1000

    OOP: Extracting duplicate logic in MortgageCalculator.java.

--- CalculateMortgage/src/main/java/com/mortgage/MortgageCalculator.java
+++ CalculateMortgage/src/main/java/com/mortgage/MortgageCalculator.java
@@ -1,18 +1,20 @@
 package com.mortgage;
 
 public class MortgageCalculator {
+    public final static byte MONTHS_IN_YEAR = 12;
+    public final static byte PERCENT = 100;
+    private int principal;
+    private float annualInterest;
+    private byte years;
     public MortgageCalculator(int principal, float annualInterest, byte years) {
         this.principal = principal;
         this.annualInterest = annualInterest;
         this.years = years;
     }
 
-    private int principal;
-    private float annualInterest;
-    private byte years;
     public double calculateBalance(short numberOfPaymentsMade) {
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = getNumberOfPayments();
         double balance = principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
@@ -21,13 +23,21 @@ public class MortgageCalculator {
     }
 
     public double calculateMortgage() {
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = getNumberOfPayments();
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
         return mortgage;
+    }
+
+    private float getMonthlyInterest() {
+        return annualInterest / PERCENT / MONTHS_IN_YEAR;
+    }
+
+    private int getNumberOfPayments() {
+        return years * MONTHS_IN_YEAR;
     }
 
     public short getYears() {
